3483001d67007e823311688f5da1770d5e7f277c
swamyuvas39 <swamyuvas@gmail.com>
2023-05-23 13:25:41 +0530

    Background color of second item changed to green and third item made invisible

--- dom.js
+++ dom.js
@@ -30,17 +30,75 @@
 //head.style.borderBottom='solid black 3px';
 
 //GET ELEMENTS BY CLASS NAME
-var items = document.getElementsByClassName('list-group-item');
-console.log(items);//gives list of items
-console.log(items[2]);//accessing items
-items[2].textContent="Hello";// changing text in the item
-items[2].style.fontWeight='bold';//changing style
-items[2].style.backgroundColor = 'green';
-//items.style.backgroundColor="green";// we cannot apply to all the items, we need to use for loop
-for (var i=0; i<items.length; i++)
-{
-    items[i].style.fontWeight='bold'; // it bold all the items
-}
+// var items = document.getElementsByClassName('list-group-item');
+// console.log(items);//gives list of items
+// console.log(items[2]);//accessing items
+// items[2].textContent="Hello";// changing text in the item
+// items[2].style.fontWeight='bold';//changing style
+// items[2].style.backgroundColor = 'green';
+// //items.style.backgroundColor="green";// we cannot apply to all the items, we need to use for loop
+// for (var i=0; i<items.length; i++)
+// {
+//     items[i].style.fontWeight='bold'; // it bold all the items
+// }
+
+//GET ELEMENT BY TAGNAME//
+
+// var li = document.getElementsByTagName('li');
+// console.log(li);//gives list of items
+// console.log(li[2]);//accessing items
+// li[2].textContent="Hello";// changing text in the item
+// li[2].style.fontWeight='bold';//changing style
+// li[2].style.backgroundColor = 'green';
+// li[4].textContent="Hello 5";
+// for (var i=0; i<li.length; i++)
+// {
+//      li[i].style.fontWeight='bold'; // it bold all the items
+// }
+
+//QUERY SELECTOR//
+
+// // when we want to use more than one items then we use query selector
+
+// var head = document.querySelector("#main-header");// we can use anything like Id,className,TagName
+// //instead of using document.querySelector, we can replace with $ symbol
+// //var head = $('#main-header');
+// head.style.borderBottom = 'solid black 4px';
+
+// // grab input 
+// var input = document.querySelector('input');
+// input.value = "Hello!, Swamyuvas";
+// var submit = document.querySelector('input[type="submit"]');
+// submit.value="SEND";
+// var item = document.querySelector('.list-group-item');//class of list 
+// item.style.color = 'green';
+// // change style to last item
+// var LastItem = document.querySelector('.list-group-item:last-child');
+// LastItem.style.color = 'red';
+// // change style to nth child
+// var SecondItem = document.querySelector('.list-group-item:nth-child(2)');
+// SecondItem.style.color = 'blue';
+
+// second item background color green
+var secondItem = document.querySelector('li:nth-child(2)');
+secondItem.style.backgroundColor = 'green';
+// Using display property
+// var thirdItem = document.querySelector('li:nth-child(2)');
+//thirdItem.style.display = 'none';
+
+// Using visibility property
+const thirdItem = document.querySelector('li:nth-child(3)');
+thirdItem.style.visibility = 'hidden';
+
+
+
+
+
+
+
+
+
+
 
 
 
