b245203f8bac33f373c36798ad2a12499b29533e
swamyuvas39 <swamyuvas@gmail.com>
2023-05-23 11:16:24 +0530

    dom js file added

---
+++ dom.js
@@ -0,0 +1,52 @@
+//console.dir(document);// it gives complete details of the document
+//console.log(document.URL);// it gives URL of the document
+//console.log(document.title);//it gives title of the document
+//document.title='swamyuvas';// changing title of the document
+//console.log(document.doctype);// <!DOCTYPE html>
+//console.log(document.head); //checking head
+//console.log(document.body);//checking body
+//console.log(document.all);// it gives all the element tags 
+//console.log(document.all[6])//accessing particular element tag
+// If u want to add some text to the element 
+//document.all[10].textContent='Hello Swamyuvas';
+//console.log(document.forms);// if want to get all the forms
+//console.log(document.links);//if want to get all the links
+// we can access forms by using []
+//console.log(document.forms[0]);
+//console.log(document.images);//it gives images that we used
+
+//GET ELEMENT BY ID
+//console.log(document.getElementById('items'));
+// we can place inside a variable
+//var items = document.getElementById('items');
+//console.log(items);
+//var headerTitle = document.getElementById('header-title');
+//var head = document.getElementById('main-header');
+// console.log(headerTitle);
+// headerTitle.textContent="Hello";
+// headerTitle.innerText="Good bye";
+// headerTitle.innerHTML='</h3>hello</h3>';
+// changes  style 
+//head.style.borderBottom='solid black 3px';
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
